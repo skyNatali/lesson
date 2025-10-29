b815925bfeb52e5a06056734fc0582c33061d03c
skyNatali <nanalu1@mail.ru>
2025-10-29 16:10:03 +0500

    Work1

---
+++ lesson_1_task_3.py
@@ -0,0 +1,10 @@
+# first_name = "Natali"
+# last_name = "Loc"
+
+# Создаем переменные и получаем значения через input
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+
+# Выводим результат в нужном формате
+# print("Вас зовут:", last_name, first_name)
+print("Вас зовут: " + first_name, last_name)
