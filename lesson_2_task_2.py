30f555d849f0e5cfa4fe7d5af5e61700a78e89d7
skyNatali <nanalu1@mail.ru>
2025-11-13 11:36:14 +0500

    task_3

---
+++ lesson_2_task_2.py
@@ -0,0 +1,9 @@
+def is_year_leap(year):
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
+year_to_check = 2024
+result = is_year_leap(year_to_check)
+print(f"год {year_to_check}: {result}")
